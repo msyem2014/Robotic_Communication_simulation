2127de10b5ec3462dadfed6dd85cf573f034210f
mhmdreda99 <Moreda491999@gmail.com>
2020-10-29 01:21:01 +0200

    1st push

---
+++ roboticsmovement/connictions.c
@@ -0,0 +1,7 @@
+#include"Def.h"
+
+void ConnectArms_ (void) {
+
+RA.S.e =& RA.E ;
+
+}
---
+++ roboticsmovement/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Finger{
+    int Volt ;
+    int degree;
+};
+struct Wrest{
+    int Volt ;
+    int degree;
+     struct Finger *f ;
+};
+struct Elbow{
+    int Volt ;
+    int degree;
+     struct Wrest *w ;
+};
+struct Shoulder{
+    int Volt ;
+    int degree;
+    struct Elbow *e ;
+};
+
+struct ARM {
+    struct Shoulder S ;
+    struct Elbow  E;
+    struct  Wrest W;
+    struct Finger F ;
+
+}RA,LA;
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
